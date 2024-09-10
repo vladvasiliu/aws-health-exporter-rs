3d72f857a26d49793deb0c13e95d6d117fa3fa97
Vlad Vasiliu <175579+vladvasiliu@users.noreply.github.com>
2024-09-10 11:13:18 +0200

    Add context to scrape errors

--- src/exporter/mod.rs
+++ src/exporter/mod.rs
@@ -1,8 +1,10 @@
 use std::sync::Arc;
 use prometheus_client::encoding::text::encode;
 
-use anyhow::{anyhow, Result};
+use anyhow::{anyhow, Context, Result};
 use aws_sdk_health::types::{OrganizationEvent};
+use axum::http::StatusCode;
+use axum::response::IntoResponse;
 use axum::Router;
 use axum::routing::get;
 use prometheus_client::encoding::EncodeLabelSet;
@@ -10,7 +12,7 @@ use prometheus_client::registry::Registry;
 use prometheus_client::metrics::gauge::Gauge;
 use prometheus_client::metrics::family::Family;
 use tokio::signal;
-use tracing::debug;
+use tracing::{debug, warn};
 use crate::scraper::Scraper;
 
 const VERSION: &str = env!("CARGO_PKG_VERSION");
@@ -28,32 +30,38 @@ impl Server {
 
         Self { address, main_registry, scraper }
     }
-    
+
     pub async fn serve(&self) -> Result<()> {
         let scraper = self.scraper.clone();
         let app = Router::new().route("/", get(|| async { format!("AWS Health Exporter v{}", VERSION) })).route("/metrics", get(|| async {
-            
-            let result = scrape(scraper).await.and_then(|registry| {
+
+            let result = scrape(scraper).await.context("failed to scrape events").and_then(|registry| {
                 let mut buffer = String::new();
-            
-                encode(&mut buffer, &registry)?;
+
+                encode(&mut buffer, &registry).context("failed to encode registry")?;
                 Ok(buffer)
             });
-                
-            result.unwrap()
+
+            match result {
+                Ok(r) => (StatusCode::OK, r),
+                Err(err) => {
+                    warn!(error = err.root_cause(), "failed to scrape events");
+                    (StatusCode::INTERNAL_SERVER_ERROR, err.to_string())
+                }
+            }
         }));
-        
+
         let listener = tokio::net::TcpListener::bind(&self.address).await?;
-        
+
         axum::serve(listener, app).with_graceful_shutdown(shutdown_signal()).await.unwrap();
-        
+
         Ok(())
     }
 }
 
 pub async fn scrape(scraper: Arc<Scraper>) -> Result<Registry> {
     let mut registry = <Registry>::default();
-    
+
     let health_events = Family::<HealthLabels, Gauge>::default();
 
 
@@ -63,9 +71,9 @@ pub async fn scrape(scraper: Arc<Scraper>) -> Result<Registry> {
     for event in events {
         let accounts = scraper.get_affected_accounts(&event).await?;
         for account in accounts {
-            let labels = HealthLabels::new(&account, &event);
+            let labels = HealthLabels::new(&account, &event)?;
             debug!("{:?}", labels);
-            health_events.get_or_create(&labels.unwrap()).set(1);
+            health_events.get_or_create(&labels).set(1);
         }
     }
     registry.register("aws_health_event", "Status of AWS Health events", health_events);
